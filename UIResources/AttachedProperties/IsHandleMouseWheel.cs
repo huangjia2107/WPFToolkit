dc7cb7c31e8aae50545cfa4bd265743421f7a63d
huangjia <654664168@qq.com>
2018-03-01 10:04:53 +0800

    modify

--- UIResources/AttachedProperties/IsHandleMouseWheel.cs
+++ UIResources/AttachedProperties/IsHandleMouseWheel.cs
@@ -4,34 +4,26 @@ namespace UIResources.AttachedProperties
 {
     public partial class AttachedProperty
     {
-        //HandleMouseWheel
-        public static RoutedEventHandler MouseWheelEventHandler = (s, e) =>
+        public static readonly DependencyProperty IsHandleMouseWheelProperty = DependencyProperty.RegisterAttached("IsHandleMouseWheel", typeof(bool?), typeof(AttachedProperty), 
+            new UIPropertyMetadata(null, IsHandleMouseWheelPropertyChangedCallback));
+        public static bool? GetIsHandleMouseWheel(DependencyObject obj)
         {
-            e.Handled = false;
-        };
-
-        public static readonly DependencyProperty IsHandleMouseWheelProperty = DependencyProperty.RegisterAttached("IsHandleMouseWheel", typeof(bool), typeof(AttachedProperty), 
-            new PropertyMetadata(true, IsHandleMouseWheelPropertyChangedCallback));
-        public static bool GetIsHandleMouseWheel(DependencyObject obj)
-        {
-            return (bool)obj.GetValue(IsHandleMouseWheelProperty);
+            return (bool?)obj.GetValue(IsHandleMouseWheelProperty);
         }
-        public static void SetIsHandleMouseWheel(DependencyObject obj, bool value)
+        public static void SetIsHandleMouseWheel(DependencyObject obj, bool? value)
         {
             obj.SetValue(IsHandleMouseWheelProperty, value);
         }
+        
         static void IsHandleMouseWheelPropertyChangedCallback(DependencyObject target, DependencyPropertyChangedEventArgs e)
         {
             var obj = target as FrameworkElement;
             if (obj == null)
                 return;
 
-            var isHandle = (bool)e.NewValue;
-
-            if (isHandle)
-                obj.RemoveHandler(UIElement.MouseWheelEvent, MouseWheelEventHandler);
-            else
-                obj.AddHandler(UIElement.MouseWheelEvent, MouseWheelEventHandler, true);
-        }
+            var isHandled = (bool?)e.NewValue;
+            if (isHandled.HasValue)
+                obj.AddHandler(UIElement.MouseWheelEvent, new RoutedEventHandler((s, args) => args.Handled = isHandled.Value), true);
+        } 
     }
 }
