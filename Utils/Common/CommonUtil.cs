af79aab05ba5af7317fd3da688ba6703c1f8a37d
huangjia <654664168@qq.com>
2018-08-27 14:12:15 +0800

    add CommonUtil

---
+++ Utils/Common/CommonUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Utils.Common
+{
+    public static class CommonUtil
+    {
+        /*
+         *    1   2   3   4             16  11   6   1
+         *    5   6   7   8             17  12   7   2
+         *    9  10  11  12      ->     18  13   8   3
+         *   13  14  15  16             19  14   9   4
+         *   17  18  19  20             20  15  10   5
+         * 
+         */
+        public static int RotateMatrixIndex(int index, int r, int c)
+        {
+            return (c - 1 - (index - 1) % c) * r + (index - 1) / c + 1;
+        }
+    }
+}
