bd9ee8c99027cfcbbb42ca8eacb54da2f79edb0f
huangjia <654664168@qq.com>
2020-08-27 16:48:55 +0800

    add ResizePathData

--- Utils/Common/CommonUtil.cs
+++ Utils/Common/CommonUtil.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Utils.Common
 {
@@ -15,6 +17,28 @@ namespace Utils.Common
         public static int RotateMatrixIndex(int index, int r, int c)
         {
             return (c - 1 - (index - 1) % c) * r + (index - 1) / c + 1;
+        }
+        
+        public static string ResizePathData(string originalData, double curWidth, double curHeight, double desiredWidth, double desiredHeight)
+        {
+            var pattern = @"^[a-zA-Z][0-9]+(\.[0-9]+)?$";
+            var rw = curWidth / desiredWidth;
+            var rh = curHeight / desiredHeight;
+            
+            var pathSpans = originalData.Split(' ');
+            
+            return string.Join(" ",pathSpans.Select(span=>
+            {
+                var pos = span.Split(',');
+                var match = Regex.IsMatch(pos[0], pattern);
+                var match1 = Regex.IsMatch(pos[1], pattern);
+                
+                return string.Format("{0}{1:#0.##},{2}{3:#0.##}",
+                              match ? pos[0].Substring(0,1) : "",
+                              double.Parse(match ? pos[0].Remove(0,1) : pos[0]) / rw,
+                              match1 ? pos[1].Substring(0,1):"",
+                              double.Parse(match1 ? pos[1].Remove(0,1) : pos[1]) / rh);
+            }).ToArray());
         }
     }
 }
